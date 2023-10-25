e45598b3d66c83b0b1832c7683855c4e2acfddb5
Kang Pyo <pyokang26@gmail.com>
2023-10-25 18:09:09 +0800

    feature(next): Enable server actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    // Allows server actions
+    serverActions: true,
+    mdxRs: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
